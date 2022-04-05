fbba0f34334176026b792ff9430698509e43dc94
ashleycodes920 <59901528+ashleycodes920@users.noreply.github.com>
2022-04-05 10:14:15 -0500

    added svg and video files

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = () => {
+ const song = document.querySelector('.song');
+ const play = document.querySelector('.play');
+ const outline = document.querySelector('.moving-outline circle');
+ const video = document.querySelector('.vid-container video');
+ 
+ //sounds
+ const sounds = document.querySelectorAll('.sound-picker button');
+ //time display
+ const timeDisplay = document.querySelector('.time-display');
+ //get the length of circle outline
+ const outlineLength = outline.getTotalLength();
+ console.log(outlineLength);
+
+}
+
+app();
